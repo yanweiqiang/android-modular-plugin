920564d0d6f5c5ba4ebd7818ad32e0b2721496dd
GitHub Action <action@github.com>
2021-08-27 08:56:03 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/yanweiqiang/androidmodularplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.yanweiqiang.androidmodularplugin.services
+
+import com.github.yanweiqiang.androidmodularplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/yanweiqiang/androidmodularplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/yanweiqiang/androidmodularplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.yanweiqiang.androidmodularplugin.services
 
+import com.github.yanweiqiang.androidmodularplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
